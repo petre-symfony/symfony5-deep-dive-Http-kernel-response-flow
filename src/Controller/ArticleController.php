73a2a4c39328fa57de540ed2b422221e8d5144df
petrero <petrero@gmail.com>
2020-05-06 13:40:37 +0300

    21.1.Custom Global Controller Arguments
    Now that we understand a lot more about its flow, we're on a mission to find weird, crazy things that we can do in Symfony. For the next one, pretend that, for some reason, we need to know whether or not a visitor is using a Mac or not. In fact, we need this info so often, that we want the ability to add an $isMac argument to any controller, like this.
    
    Let's dump()... and then try it. No surprise, it explodes!
    
      Controller show() requires that you provide a value for the  argument.
    
    I'll go back to a real article page, though that won't make any difference.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -42,7 +42,8 @@ class ArticleController extends AbstractController {
   /**
    * @Route("/news/{slug}", name="article_show")
    */
-  public function show($slug, SlackClient $slack, ArticleRepository $articleRepository) {
+  public function show($slug, SlackClient $slack, ArticleRepository $articleRepository, $isMac) {
+    dump($isMac);
   	$article = $articleRepository->findOneBy(['slug' => $slug]);
 
   	if(!$article){
